09d9fd144127958fe513e60fcb21548fb714a22b
Ceschetto <francesco.bocci09@gmail.com>
2023-08-15 22:12:06 +0200

    Iniziato un design per il player audio. Non sono convinto dell'utilizzo di 2 box, ma non so se conviene usare una griglia per evitare problemi di visualizzazione. Capire come ottenere l'input dal campo di entry Risolvere il warning critico riguardante la entry (~ pango cursor out of bounds)

--- src/gtk_interface.c
+++ src/gtk_interface.c
@@ -17,6 +17,28 @@ void draw_app(GtkApplication *app)
     gtk_window_set_title(GTK_WINDOW(window), "Ciao Bellezza");
     gtk_window_set_default_size(GTK_WINDOW(window), 500, 500);
 
+    //grande box che conterrà tutti ciò che servirà
+    GtkWidget *bigBox = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 2);
+    gtk_window_set_child(GTK_WINDOW(window), bigBox);
+    //conterra l'enty per poter inserire la cartella e sotto comparirà lista con i nomi dei file
+    GtkWidget *rightBox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 2);
+    //griglia che conterrà il nostro player audio con i bottoni e se dio vuole anche una specie di visualizzatore
+    GtkWidget *leftGrid = gtk_grid_new();
+
+    gtk_box_append(GTK_BOX(bigBox), rightBox);
+    gtk_box_append(GTK_BOX(bigBox), leftGrid);
+
+
+    GtkEntryBuffer *EntryTextBuffer = gtk_entry_buffer_new("Inserire percorso cartella audio", 34);
+    GtkWidget *textEntry = gtk_entry_new_with_buffer( EntryTextBuffer );
+
+    gtk_box_append(GTK_BOX(rightBox), textEntry);
+
+
+
+
+
+
     g_print("\n The number of file found is: %d\n", count_number_of_file("."));
     print_FNList(enlist_files_name("."));
 
